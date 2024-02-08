30cd62f6b0096aa885c418ca1e6fdbe1937ea098
Bakii <tayolowosale@gmail.com>
2024-02-08 12:12:43 +0100

    env change

--- sanity/env.js
+++ sanity/env.js
@@ -4,4 +4,4 @@ export const apiVersion =
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 export const useCdn = false
-export const token = process.env.NEXT_PUBLIC_SANITY_TOKEN
+export const token = process.env.SANITY_TOKEN
